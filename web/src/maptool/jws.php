7a9adc13a13de84d1ba813b39d9414fab5e345e3
michaelrice <michaelrice>
2005-03-27 03:43:07 +0000

    Converted jnlp links to use jws.php - dynamic jnlp url generator, so that resulting jnlp will point to it's own location for the source of files.
    This will allow 'beta' site to contain the application separate from the
    production site.  (and other test sites as well).

--- web/src/maptool/jws.php
+++ web/src/maptool/jws.php
@@ -1,11 +1,15 @@
 <?php
 header("Content-type: application/x-java-jnlp-file");
+if ( $_REQUEST['debug'] ) {
+    header("Content-type: text/plain");
+}
 $bits = pathinfo( $_SERVER['REQUEST_URI'] );
 $base = sprintf("http://%s/",  $_SERVER['SERVER_NAME']);
 $self = sprintf("http://%s%s", $_SERVER['SERVER_NAME'],$bits['dirname']);
 echo "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n";
 ?>
-	<jnlp codebase="<? echo $self ?>">
+	<jnlp
+    spec="1.5+" codebase="<? echo $self ?>">
 	<information>
 		<title>RPTools MapTool</title>
 		<vendor><? echo $base ?></vendor>
@@ -17,7 +21,7 @@ echo "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n";
 	</security>
 	
 	<resources>
-		<j2se version="1.5+" />
+		<j2se version="1.5+" java-vm-args="-Xms64m -Xmx128m"/>
 		
 		<jar href="maptool-1.0M1.b7.jar" />
 		<jar href="lib/clientserver-1.0.b4.jar" />		
