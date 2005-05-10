436bdc1d21cb3cfe523601e521e3c1fdc0bc1927
tcroft <tcroft>
2005-05-10 18:35:20 +0000

    remove debugging

--- tokentool/src/net/rptools/tokentool/TokenCompositionPanel.java
+++ tokentool/src/net/rptools/tokentool/TokenCompositionPanel.java
@@ -1,5 +1,5 @@
 /*
- * $Id: TokenCompositionPanel.java,v 1.4 2005/05/10 18:29:04 tcroft Exp $
+ * $Id: TokenCompositionPanel.java,v 1.5 2005/05/10 18:35:20 tcroft Exp $
  *
  * Copyright (C) 2005, Digital Motorworks LP, a wholly owned subsidiary of ADP.
  * The contents of this file are protected under the copyright laws of the
@@ -140,7 +140,7 @@ public class TokenCompositionPanel extends JComponent implements DropTargetListe
         dtde.acceptDrop(DnDConstants.ACTION_COPY_OR_MOVE);
         
         if (transferable.isDataFlavorSupported(DataFlavors.IMAGE_FLAVOR)) {
-            System.out.println ("Using image");
+
             try {
                 setToken((BufferedImage) transferable.getTransferData(DataFlavors.IMAGE_FLAVOR));
                 return;
