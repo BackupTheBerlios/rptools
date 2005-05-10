b17c88a5c62c527d635d32c3bbba90c65f3ef4c4
tcroft <tcroft>
2005-05-10 18:28:48 +0000

    *** empty log message ***

---
+++ rplib/src/net/rptools/common/DataFlavors.java
@@ -0,0 +1,18 @@
+/*
+ * $Id: DataFlavors.java,v 1.1 2005/05/10 18:28:48 tcroft Exp $
+ *
+ * Copyright (C) 2005, Digital Motorworks LP, a wholly owned subsidiary of ADP.
+ * The contents of this file are protected under the copyright laws of the
+ * United States of America with all rights reserved. This document is
+ * confidential and contains proprietary information. Any unauthorized use or
+ * disclosure is expressly prohibited.
+ */
+package net.rptools.common;
+
+import java.awt.datatransfer.DataFlavor;
+
+public class DataFlavors {
+
+    public static final DataFlavor IMAGE_FLAVOR = new DataFlavor("image/x-java-image; class=java.awt.Image", "Image");
+    
+}
